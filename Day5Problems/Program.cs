381986f53b09bf752e17918cb4855cc59da392f0
Vivek Verma <vverma240420@gmail.com>
2023-02-21 21:47:32 +0530

    Day 5 Practice Problems

---
+++ Day5Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day5Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 5 Practice Problem");
+        }
+    }
+}
