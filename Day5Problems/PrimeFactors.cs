78c8673a5aa9f7c6241d323df0a968ff06062bce
Vivek Verma <vverma240420@gmail.com>
2023-02-21 22:29:16 +0530

    Adding Prime Factor Problem

---
+++ Day5Problems/PrimeFactors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Problems
+{
+    internal class PrimeFactors
+    {
+        public static void PrimeFactor()
+        {
+            Console.WriteLine("Enter the Number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i, j;
+            int count = 0;
+            Console.Write("Prime factor of {0}:\n1\n", n);
+            for (i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count = 0;
+                    for (j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        Console.WriteLine(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
