613736b5d78e2e7cb2fbd01751fdb68f7dce7a9b
Vivek Verma <vverma240420@gmail.com>
2023-02-22 15:14:08 +0530

    Adding Swap Two Number Program

--- Day5Problems/Program.cs
+++ Day5Problems/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:PowerOfTwo\n4:HarmonicNumber\n5:RemainderAndQuotient\n6:PrimeFactors\n11:Exit");
+            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:PowerOfTwo\n4:HarmonicNumber\n5:RemainderAndQuotient\n6:PrimeFactors\n7:SwapTwoNumber\n11:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -27,6 +27,9 @@
                     break;
                 case 6:
                     PrimeFactors.PrimeFactor();
+                    break;
+                case 7:
+                    SwapTwoNumber.SwapNumbers();
                     break;
                 default:
                     Console.WriteLine("choose the above options");
---
+++ Day5Problems/SwapTwoNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Day5Problems
+{
+    internal class SwapTwoNumber
+    {
+        public static void SwapNumbers()
+        {
+            Console.WriteLine("Enter a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            int b = Convert.ToInt32(Console.ReadLine());          
+            a = a + b; //a=15 (5+10)      
+            b = a - b; //b=5 (15-10)      
+            a = a - b; //a=10 (15-5)   
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
