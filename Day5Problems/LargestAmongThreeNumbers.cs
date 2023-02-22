d0f3669034ea19e5e43b72d3bc3b6986d190144c
Vivek Verma <vverma240420@gmail.com>
2023-02-22 15:43:30 +0530

    Find Largest Among Three Numbers Program

---
+++ Day5Problems/LargestAmongThreeNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Problems
+{
+    internal class LargestAmongThreeNumbers
+    {
+        public static void FindingLargeNum()
+        {
+            Console.WriteLine("Enter the Number1");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number3");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("number1 is the Largest one");
+                }
+                else
+                {
+                    Console.WriteLine("number3 is the Largest one");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("number2 is the Largest one");
+            }
+            else
+            {
+                Console.WriteLine("number3 is the Largest one");
+            }
+        }
+
+    }
+}
--- Day5Problems/Program.cs
+++ Day5Problems/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:PowerOfTwo\n4:HarmonicNumber\n5:RemainderAndQuotient\n6:PrimeFactors\n7:SwapTwoNumber\n8:EvenOrOdd\n9:VowelOrConsonant\n11:Exit");
+            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:PowerOfTwo\n4:HarmonicNumber\n5:RemainderAndQuotient\n6:PrimeFactors\n7:SwapTwoNumber\n8:EvenOrOdd\n9:VowelOrConsonant\n10:LargestAmongThreeNumbers\n11:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -36,6 +36,9 @@
                     break;
                 case 9:
                     VowelOrConsonant.VowelOrConsonantCheck();
+                    break;
+                case 10:
+                    LargestAmongThreeNumbers.FindingLargeNum();
                     break;
                 default:
                     Console.WriteLine("choose the above options");
