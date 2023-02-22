c7ccacc478adec4913c44c41e51f5d4dd9dc9cef
Vivek Verma <vverma240420@gmail.com>
2023-02-22 15:23:05 +0530

    Adding Even Or Odd Program

---
+++ Day5Problems/EvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Problems
+{
+    internal class EvenOrOdd
+    {
+        public static void EvenOrOddCheck()
+        {
+
+            Console.WriteLine("Enter The Number To Check");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+
+                Console.WriteLine("Number is even:" + n);
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd:" + n);
+            }
+        }
+    }
+}
